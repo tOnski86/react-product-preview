9429a2d5b7aff9bf4dde763f0415657b730a35b5
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-10 05:36:01 +0800

    Add static assets, setup default styles in Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1 className='text-cyan-600'>Hello React!</h1>;
+  return <h1 className='text-blue font-fraunces'>Hello React!</h1>;
 }
 
 export default App;
+
+// white: 'hsl(0, 0%, 100%)',
+// cream: 'hsl(30, 38%, 92%)',
+// green: 'hsl(158, 36%, 37%)',
+// grey: 'hsl(228, 12%, 48%)',
+// blue: 'hsl(212, 21%, 14%)',
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fraunces: ['Fraunces', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      cream: 'hsl(30, 38%, 92%)',
+      green: 'hsl(158, 36%, 37%)',
+      grey: 'hsl(228, 12%, 48%)',
+      blue: 'hsl(212, 21%, 14%)',
+    },
   },
   plugins: [],
 };
